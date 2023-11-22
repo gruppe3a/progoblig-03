9705988594a63ae1fd1250cb99a7d99428538ef4
noaho04 <noah@ofsti.com>
2023-11-22 19:48:21 +0100

    Add files via upload

--- js/noah.js
+++ js/noah.js
@@ -1,12 +1,3 @@
-const par = document.getElementById("noahFigureSVG")
-const bg = rectangleSVG(160, 160, "solid", "darkred")
-const outer_circ = circleSVG(60, "outline", "yellow")
-const inner_circ = circleSVG(59, "solid", "black")
-
-par.appendChild(bg)
-par.appendChild(outer_circ)
-par.appendChild(inner_circ)
-
 const parC = document.getElementById("noahFigureC") 
 const bgC = rectangle(160, 160, "solid", "darkred")
 const outer_circC = circle(60, "outline", "yellow")
@@ -15,3 +6,13 @@ const inner_circC = circle(59, "solid", "black")
 parC.appendChild(bgC)
 parC.appendChild(outer_circC)
 parC.appendChild(inner_circC)
+
+const pair = document.getElementById("noahFigureSVG")
+const bg = rectangleSVG(160, 160, "solid", "darkred")
+const outer_circ = circleSVG(60, "outline", "yellow")
+const inner_circ = circleSVG(59, "solid", "black")
+
+pair.appendChild(bg)
+pair.appendChild(outer_circ)
+pair.appendChild(inner_circ)
+
