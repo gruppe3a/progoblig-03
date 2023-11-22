0e5af4b72d9e675f569adaf286273c6d0d9e7422
noaho04 <noah@ofsti.com>
2023-11-22 22:52:36 +0100

    fix på min

--- js/noah.js
+++ js/noah.js
@@ -1,25 +1,3 @@
-/*const parC = document.getElementById("noahFigureC") 
-const bgC = rectangle(160, 160, "solid", "darkred")
-const outer_circC = circle(60, "outline", "yellow")
-outer_circC.classList.add("outer-circ")
-const inner_circC = circle(59, "solid", "black")
-inner_circC.classList.add("inner-circ")
-
-parC.appendChild(bgC)
-parC.appendChild(outer_circC)
-parC.appendChild(inner_circC)
-
-const pair = document.getElementById("noahFigureSVG")
-const bg = rectangleSVG(160, 160, "solid", "darkred")
-const outer_circ = circleSVG(60, "outline", "yellow")
-outer_circ.classList.add("outer-circ")
-const inner_circ = circleSVG(59, "solid", "black")
-inner_circ.classList.add("inner-circ")
-
-pair.appendChild(bg)
-pair.appendChild(outer_circ)
-pair.appendChild(inner_circ)
-*/
 const pairSVG = document.getElementById("noahFigureSVG") 
 const bgSVG = rectangleSVG(160, 160, "solid", "darkred")
 bgSVG.classList.add("bg")
