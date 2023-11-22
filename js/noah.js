d24a9ef3872fbde6c475bcb5d8088c76126e83f0
noaho04 <noah@ofsti.com>
2023-11-22 21:02:05 +0100

    + noah

--- js/noah.js
+++ js/noah.js
@@ -1,4 +1,4 @@
-const parC = document.getElementById("noahFigureC") 
+/*const parC = document.getElementById("noahFigureC") 
 const bgC = rectangle(160, 160, "solid", "darkred")
 const outer_circC = circle(60, "outline", "yellow")
 const inner_circC = circle(59, "solid", "black")
@@ -15,4 +15,35 @@ const inner_circ = circleSVG(59, "solid", "black")
 pair.appendChild(bg)
 pair.appendChild(outer_circ)
 pair.appendChild(inner_circ)
+*/
+const pairSVG = document.getElementById("noahFigureSVG") 
+const bgSVG = rectangleSVG(160, 160, "solid", "darkred")
+bgSVG.classList.add("bg")
 
+const outercircleSVG = circleSVG(60,  "outline", "yellow")
+outercircleSVG.classList.add("outercircle")
+
+const innercircleSVG = circleSVG(59, "solid", "black")
+innercircleSVG.classList.add("innercircle")
+
+pairSVG.appendChild(bgSVG)
+pairSVG.appendChild(outercircleSVG)
+pairSVG.appendChild(innercircleSVG)
+
+
+
+
+
+const pairCanvas = document.getElementById("noahFigureC") 
+const bg = rectangle(160, 160, "solid", "darkred")
+bg.classList.add("bgC")
+
+const outercircle = circle(60,  "outline", "yellow")
+outercircle.classList.add("outercircleC")
+
+const innercircle = circle(59, "solid", "black")
+innercircle.classList.add("innercircleC")
+
+pairCanvas.appendChild(bg)
+pairCanvas.appendChild(outercircle)
+pairCanvas.appendChild(innercircle)
